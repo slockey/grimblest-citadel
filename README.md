353c1799ec0e9c2c9d415585e1ef38ae4310cc4b
Steven Lockey <steven.lockey@cision.com>
2021-06-18 15:03:11 -0400

    initial commit after setting up env and deps as described in part-0 of the tutorial

---
+++ README.md
@@ -0,0 +1,4 @@
+# Roguelike tutorial written in Pyhton3 and TCOD
+
+Site link: http://rogueliketutorials.com/tutorials/tcod/v2/
+
---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import tcod
+
+
+def main():
+    print("harlow wold")
+
+
+if __name__ == "__main__":
+    main()
