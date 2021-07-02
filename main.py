0658e254d2901a4ea05399b6e5490670231f2553
Steven Lockey <sclockey@gmail.com>
2021-07-02 15:03:16 -0400

    part-2 phase-1 + split out engine tasks from main + defined entity class to serve as the basis of things in-game + removed functionality that was moved to the engine

--- main.py
+++ main.py
@@ -2,21 +2,26 @@
 
 import tcod
 
-from actions import EscapeAction, MovementAction
+#from actions import EscapeAction, MovementAction
+from engine import Engine
+from entity import Entity
 from input_handlers import EventHandler
 
 def main() -> None:
     screen_width = 80
     screen_height = 50
 
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
-
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
 
-    event_hander = EventHandler()
+    event_handler = EventHandler()
+
+    player = Entity(int(screen_width/2), int(screen_height/2), "@", (255,255,255))
+    npc = Entity(int(screen_width/2-5), int(screen_height/2), "@", (255,255,0))
+    entities = {npc, player}
+
+    engine = Engine(entities=entities, event_handler=event_handler, player=player)
 
     with tcod.context.new_terminal(
         screen_width,
@@ -27,25 +32,10 @@ def main() -> None:
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
-            root_console.print(x=player_x, y=player_y, string="@")
 
-            context.present(root_console)
-
-            # clear the console so we can refresh with new state
-            root_console.clear()
-
-            for event in tcod.event.wait():
-                action = event_hander.dispatch(event)
-
-                if action is None:
-                    continue
-                
-                if isinstance(action, MovementAction):
-                    player_x += action.dx
-                    player_y += action.dy
-                
-                elif isinstance(action, EscapeAction):
-                    raise SystemExit()
+            engine.render(console=root_console, context=context)
+            events = tcod.event.wait()
+            engine.handle_events(events)
 
 
 if __name__ == "__main__":
