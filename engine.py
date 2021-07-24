a500c8b72ea30050daf1a13d8286029b9f624d59
Steven Lockey <sclockey@gmail.com>
2021-07-24 16:00:52 -0400

    Refactored code + event handlers now implement handle_events ... not the engine + game map now has a reference to the engine + entities now have a reference to the game map + actions are initialized with the entity doing the action

--- engine.py
+++ engine.py
@@ -1,38 +1,32 @@
 
-from typing import Set, Iterable, Any
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
 
 from tcod.context import Context
 from tcod.console import Console
 from tcod.map import compute_fov
 
-from actions import EscapeAction, MovementAction
-from entity import Entity
-from game_map import GameMap
 from input_handlers import EventHandler
 
+if TYPE_CHECKING:
+    from entity import Entity
+    from game_map import GameMap
+
+
 class Engine:
+
+    game_map: GameMap
+
     # engine to take over some duties from main.py
-    def __init__(self, event_handler: EventHandler, game_map: GameMap, player: Entity):
-        self.event_handler = event_handler
-        self.game_map = game_map
+    def __init__(self, player: Entity):
+        self.event_handler: EventHandler = EventHandler(self)
         self.player = player
-        self.update_fov()
+
 
     def handle_enemy_turns(self) -> None:
         for entity in self.game_map.entities - {self.player}:
             print(f'The {entity.name} wonders when it will get to take a real turn.')
-
-    def handle_events(self, events: Iterable[Any]) -> None:
-        for event in events:
-            action = self.event_handler.dispatch(event)
-
-            if action is None:
-                continue
-
-            action.perform(self, self.player)
-            self.handle_enemy_turns()
-
-            self.update_fov()   # update the FOV before the player next action
 
 
     def update_fov(self) -> None:
--- game_map.py
+++ game_map.py
@@ -10,12 +10,14 @@ from tcod.console import Console
 import tile_types
 
 if TYPE_CHECKING:
+    from engine import Engine
     from entity import Entity
 
 
 class GameMap:
 
-    def __init__(self, width: int, height: int, entities: Iterable[Entity] = ()):
+    def __init__(self, engine: Engine, width: int, height: int, entities: Iterable[Entity] = ()):
+        self.engine = engine
         self.width = width
         self.height = height
         self.entities = set(entities)
@@ -28,7 +30,7 @@ class GameMap:
 
     def get_blocking_entity_at_location(self, location_x: int, location_y: int) -> Optional[Entity]:
         for entity in self.entities:
-            if entity.blocks_movement and entity.x == location_x and entity.y == location_y:
+            if (entity.blocks_movement and entity.x == location_x and entity.y == location_y):
                 return entity
         return None
 
@@ -43,10 +45,10 @@ class GameMap:
         If it isn't, but it's in the explored array then draw it with the dark color.
         Otherwise the default is SHROUD
         """
-        console.tiles_rgb[0:self.width, 0:self.height] = np.select(
+        console.tiles_rgb[0 : self.width, 0 : self.height] = np.select(
             condlist=[self.visible, self.explored], 
             choicelist=[self.tiles["light"], self.tiles["dark"]],
-            default=tile_types.SHROUD
+            default=tile_types.SHROUD,
         )
 
         for entity in self.entities:
