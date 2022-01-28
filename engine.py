d4019b7e099593eff79ccd4b8f87ebc8bf548863
Steven Lockey <sclockey@gmail.com>
2022-01-28 17:24:04 -0500

    Player can now travel down stairs to lower levels of the dungeon Current player level depth is displayed below the HP bar

--- engine.py
+++ engine.py
@@ -12,7 +12,8 @@ from tcod.console import Console
 from tcod.map import compute_fov
 
 from message_log import MessageLog
-from render_functions import render_bar, render_names_at_mouse_location
+
+import render_functions
 
 if TYPE_CHECKING:
     from entity import Actor
@@ -58,14 +59,25 @@ class Engine:
 
         self.message_log.render(console, x=21, y=45, width=40, height=5)
 
-        render_bar(
+        render_functions.render_bar(
             console=console,
             current_value=self.player.fighter.hp,
             maximum_value=self.player.fighter.max_hp,
             total_width=20
         )
 
-        render_names_at_mouse_location(console=console, x=21, y=44, engine=self)
+        render_functions.render_dungeon_level(
+            console=console,
+            dungeon_level=self.game_world.current_floor,
+            location=(0, 47),
+        )
+
+        render_functions.render_names_at_mouse_location(
+            console=console,
+            x=21,
+            y=44,
+            engine=self
+        )
 
 
     def save_as(self, filename: str) -> None:
