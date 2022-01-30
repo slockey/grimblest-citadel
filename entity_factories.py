57af1d6602ae8adb92b03450e6ee0cca75c04c50
Steven Lockey <sclockey@gmail.com>
2022-01-30 15:11:11 -0500

    Added some kind of leveling to the player + orcs/trolls provide xp when killed + player now has levels + when player level increases they select one stat to increase (hp, power, defense)

--- entity_factories.py
+++ entity_factories.py
@@ -1,8 +1,10 @@
 
+from components import level
 from components.ai import HostileEnemy
 from components import consumable
 from components.fighter import Fighter
 from components.inventory import Inventory
+from components.level import Level
 from entity import Actor, Item
 
 player = Actor(
@@ -11,7 +13,8 @@ player = Actor(
     name="Player",
     ai_cls=HostileEnemy,
     fighter=Fighter(hp=30, defense=2, power=5),
-    inventory=Inventory(capacity=26)
+    inventory=Inventory(capacity=26),
+    level=Level(level_up_base=200)
 )
 
 orc = Actor(
@@ -20,7 +23,8 @@ orc = Actor(
     name="Orc",
     ai_cls=HostileEnemy,
     fighter=Fighter(hp=10, defense=0, power=3),
-    inventory=Inventory(capacity=0)
+    inventory=Inventory(capacity=0),
+    level=Level(xp_given=35)
 )
 
 troll = Actor(
@@ -29,7 +33,8 @@ troll = Actor(
     name="Troll",
     ai_cls=HostileEnemy,
     fighter=Fighter(hp=16, defense=1, power=4),
-    inventory=Inventory(capacity=0)
+    inventory=Inventory(capacity=0),
+    level=Level(xp_given=100)
 )
 
 health_potion = Item(
