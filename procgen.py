8688b476f75b3e7a23d7da62b044d5b336371ca6
Steven Lockey <sclockey@gmail.com>
2022-02-21 16:49:39 -0500

    Now determine items and enemies by random weighting per floor

--- procgen.py
+++ procgen.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 import random
 
-from typing import Iterator, List, Tuple, TYPE_CHECKING
+from typing import Dict, Iterator, List, Tuple, TYPE_CHECKING
 
 import tcod
 from tcod import los
@@ -16,6 +16,7 @@ import tile_types
 
 if TYPE_CHECKING:
     from engine import Engine
+    from entity import Entity
 
 
 max_items_by_floor = [
@@ -28,6 +29,21 @@ max_monsters_by_floor = [
     (4,3),
     (6,5),
 ]
+
+# ordinal for the dictionaries is the dungeon depth
+# also, list of tuples means we can add multiple entities per depth
+item_chances: Dict[int, List[Tuple[Entity, int]]] = {
+    0: [(entity_factories.health_potion, 35)],
+    2: [(entity_factories.confusion_scroll, 10)],
+    4: [(entity_factories.lightning_scroll, 25)],
+    6: [(entity_factories.fireball_scroll, 25)],
+}
+enemy_chances: Dict[int, List[Tuple[Entity, int]]] = {
+    0: [(entity_factories.orc, 80)],
+    3: [(entity_factories.troll, 15)],
+    5: [(entity_factories.troll, 30)],
+    7: [(entity_factories.troll, 60)],
+}
 
 
 class RectangularRoom:
@@ -71,6 +87,33 @@ def get_max_value_for_floor(weighted_chances_by_floor: List[Tuple[int,int]], flo
     return current_value
 
 
+def get_entities_at_random(
+    weighted_chances_by_floor: Dict[int, List[Tuple[Entity, int]]],
+    number_of_entities: int,
+    floor: int,
+) -> List[Entity]:
+    entity_weighted_chances = {}
+
+    for key, values in weighted_chances_by_floor.items():
+        if key > floor:
+            break
+        else:
+            for value in values:
+                entity = value[0]
+                weighted_chance = value[1]
+
+                entity_weighted_chances[entity] = weighted_chance
+    
+    entities = list(entity_weighted_chances.keys())
+    entity_weighted_chance_values = list(entity_weighted_chances.values())
+
+    chosen_entities = random.choices(
+        entities, weights=entity_weighted_chance_values, k=number_of_entities
+    )
+
+    return chosen_entities
+
+
 def tunnel_between(start: Tuple[int, int], end: Tuple[int, int]) -> Iterator[Tuple[int, int]]:
     """Return an L-shaped tunnel between these two points"""
     x1, y1 = start
@@ -101,32 +144,16 @@ def place_entities(room: RectangularRoom, dungeon: GameMap, floor_number: int) -
         get_max_value_for_floor(max_items_by_floor, floor_number)
     )
 
-    for i in range(number_monsters):
+    monsters: List[Entity] = get_entities_at_random(enemy_chances, number_monsters, floor_number)
+
+    items: List[Entity] = get_entities_at_random(item_chances, number_items, floor_number)
+
+    for entity in monsters + items:
         x = random.randint(room.x1 + 1, room.x2 - 1)
         y = random.randint(room.y1 + 1, room.y2 - 1)
 
         if not any(entity.x == entity.y for entity in dungeon.entities):
-            if random.random() < 0.8:
-                entity_factories.orc.spawn(dungeon, x, y)
-            else:
-                entity_factories.troll.spawn(dungeon, x, y)
-
-    for i in range(number_items):
-        x = random.randint(room.x1 + 1, room.x2 - 1)
-        y = random.randint(room.y1 + 1, room.y2 - 1)
-
-        if not any(entity.x == entity.y for entity in dungeon.entities):
-            #entity_factories.health_potion.spawn(dungeon, x, y)
-            item_chance = random.random()
-
-            if item_chance < 0.7:
-                entity_factories.health_potion.spawn(dungeon, x, y)
-            elif item_chance < 0.8:
-                entity_factories.fireball_scroll.spawn(dungeon, x, y)
-            elif item_chance < 0.9:
-                entity_factories.confusion_scroll.spawn(dungeon, x, y)
-            else:
-                entity_factories.lightning_scroll.spawn(dungeon, x, y)
+            entity.spawn(dungeon, x, y)
 
 
 def generate_dungeon(
